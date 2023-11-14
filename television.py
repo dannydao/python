8d00f036869d3b343653fb5b76b5d43e3d901da3
dannydao <dannyhoangdao@gmail.com>
2023-11-13 20:44:19 -0600

    added functions

--- television.py
+++ television.py
@@ -5,9 +5,31 @@ class Television:
     MIN_CHANNEL = 0
     MAX_CHANNEL = 3
 
-def __init__(self):
-    # Instance varibles
-    self.__status = False
-    self.__muted = False
-    self.__volume = Television.MIN_VOLUME
-    self.__channel = Television.MIN_CHANNEL
+    def __init__(self):
+        # Instance varibles
+        self.__status = False
+        self.__muted = False
+        self.__volume = Television.MIN_VOLUME
+        self.__channel = Television.MIN_CHANNEL
+
+    def power(self):
+        self.__status = not self.__status   # self.__status = True
+        pass
+
+    def mute(self):
+        pass
+    
+    def channel_up(self):
+        pass
+
+    def channel_down(self):
+        pass
+
+    def volume_up(self):
+        pass
+
+    def volume_down(self):
+        pass
+
+    def __str___(self):
+        pass
